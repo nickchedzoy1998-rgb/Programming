ebbfc4006c777b89ab9391901d8f021e40b4e97e
nickchedzoy-design <nick.chedzoy@predictx.com>
2026-04-10 21:33:15 +0100

    Implement Book, Checklist, Customer, and Cable classes with constructors; define Pet class and new_pet function

--- HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-05_book/src/book.py
+++ HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-05_book/src/book.py
@@ -1,1 +1,32 @@
+"""
+Write a class named Book with the attributes name, author, genre and year, along with a constructor which assigns initial values to these attributes.
+
+Your class should work like this:
+
+python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+
+print(f"{python.author}: {python.name} ({python.year})")
+print(f"The genre of the book {everest.name} is {everest.genre}")
+Sample output
+Luciano Ramalho: Fluent Python (2015)
+The genre of the book High Adventure is autobiograp
+"""
+
 # Write your solution here:
+class Book:
+    def __init__(self, name: str, author: str, genre: str, year: int):
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.year = year
+
+    def show_details(self):
+        print(f'The genre of the book {self.name} is {self.genre}, written by {self.author} and released in {self.year}.')
+
+if __name__ == '__main__':
+    python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+    everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+
+    python.show_details()
+    everest.show_details()
