ba5118eb06dd18007dee8376d840a056617cb4f2
nickchedzoy-design <nick.chedzoy@predictx.com>
2026-04-11 15:26:14 +0100

    Refactor older_book function and update docstring with usage examples

--- HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-08_older_book/src/older_book.py
+++ HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-08_older_book/src/older_book.py
@@ -1,3 +1,21 @@
+"""
+Please write a function named older_book(book1: Book, book2: Book),
+which takes two objects of type Book as its arguments. 
+The function should print out a message with the details of whichever is the older. 
+It should print out a different message if the two books were written in the same year. 
+Please see the examples below.
+
+python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+
+older_book(python, everest)
+older_book(python, norma)
+Sample output
+High Adventure is older, it was published in 1956
+Fluent Python and Norma were published in 2015
+"""
+
 # DO NOT CHANGE CLASS Book!
 # Write your solution after the class!
 
@@ -11,3 +29,21 @@ class Book:
 # -----------------------------
 # Write your solution here
 # -----------------------------
+
+def older_book(book1: Book, book2: Book):
+    if book1.year < book2.year:
+        print(f'{book1.name} is older, it was published in {book1.year}.')
+        print(f'{book2.name} was published in {book2.year}.')
+    elif book1.year == book2.year:
+        print(f'{book1.name} and {book2.name} were both published in {book1.name}.')
+    else:
+        print(f'{book2.name} is older, it was published in {book2.year}.')
+        print(f'{book1.name} was published in {book1.year}.')
+
+
+if __name__ == '__main__':
+    python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+    everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+    norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+
+    older_book(python, everest)
