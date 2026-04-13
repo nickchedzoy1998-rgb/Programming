4867428a3d8dc34dbca9b742467659422cfd4b41
nickchedzoy-design <nick.chedzoy@predictx.com>
2026-04-13 14:54:59 +0100

    Implement books_of_genre function to filter books by genre; add usage examples in docstring

--- HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-09_books_of_genre/src/books_of_genre.py
+++ HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-09_books_of_genre/src/books_of_genre.py
@@ -1,3 +1,21 @@
+"""
+Please write a function named books_of_genre(books: list, genre: str) 
+which takes a list of objects of type Book and a string representing a genre as its arguments.
+
+The function should return a new list, which contains the books with the desired genre from 
+the original list. Please see the examples below.
+
+python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+
+books = [python, everest, norma, Book("The Snowman", "Jo Nesbø", "crime", 2007)]
+
+print("Books in the crime genre:")
+for book in books_of_genre(books, "crime"):
+    print(f"{book.author}: {book.name}")
+"""
+
 # DO NOT CHANGE CLASS Book!
 # Write your solution after the class!
 
@@ -16,3 +34,25 @@ class Book:
 # -----------------------------
 # Write your solution here
 # -----------------------------
+def books_of_genre(books: list, genre: str):
+    matching_books = []
+
+    for book in books:
+        if book.genre == genre:
+            matching_books.append(book)
+
+    if matching_books:
+        print(f'Books in the {genre} genre:')
+        for match in matching_books:
+            print(match.__repr__())
+    else:
+        print(f'There are no books in the {genre} genre')
+    
+
+if __name__ == '__main__':
+    python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+    everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+    norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+    books = [python, everest, norma, Book("The Snowman", "Jo Nesbø", "crime", 2007)]
+
+    books_of_genre(books, 'crime')
--- HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-12_number_stats/src/number_stats.py
+++ HelsinkiUni_MOOC_AdvancedPython/tmcdata/mooc-programming-26/part08-12_number_stats/src/number_stats.py
@@ -2,9 +2,45 @@
 class  NumberStats:
     def __init__(self):
         self.numbers = 0
+        self.sum = 0
 
     def add_number(self, number:int):
-        pass
+        self.numbers += 1
+        self.sum += number
 
     def count_numbers(self):
-        pass
+        return self.numbers
+    
+    def get_sum(self):
+        if self.numbers > 0:
+            return self.sum
+        else:
+            return 0
+    
+    def average(self):
+        if self.numbers > 0:
+            return self.sum / self.numbers
+        else:
+            return 0
+
+stats = NumberStats()
+even_numbers = NumberStats()
+odd_numbers = NumberStats()
+
+print('Type in your Numbers:\n')
+
+while True:
+    number = int(input('Enter a Number: '))
+
+    if number == -1:
+        break
+    stats.add_number(number)
+    if number % 2 == 0:
+        even_numbers.add_number(number)
+    else:
+        odd_numbers.add_number(number)
+
+print(f'Sum of Numbers: {stats.get_sum()}')
+print(f'Mean of Numbers: {stats.average()}')
+print(f'Sum of even numbers: {even_numbers.get_sum()}')
+print(f'Sum of odd numbers: {odd_numbers.get_sum()}')
